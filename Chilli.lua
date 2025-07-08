fd376fcbcbea8d5332b7fa3a839ee0c442f5295c
tienkhanh1 <108287881+tienkhanh1@users.noreply.github.com>
2025-07-08 20:33:11 +0700

    Update Chilli.lua

--- Chilli.lua
+++ Chilli.lua
@@ -1,11 +1,16 @@
--- Danh sách PlaceId được phép load
-local allowedPlaceIds = {
-    [109983668079237] = true,
-    [96342491571673]  = true,
+local blockedPlaceIds = {
+    [85451850104706]    = true,
+    [120437750477306]   = true,
+    [127298957482489]   = true,
+    [91820643213706]    = true,
+    [135744300508963]   = true,
+    [74982515691410]    = true,
+    [96358739341684]    = true,
+    [121424241225362]   = true,
+    [84547852189035]    = true,
 }
 
--- Kiểm tra và load script
-if allowedPlaceIds[game.PlaceId] then
+if not blockedPlaceIds[game.PlaceId] then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/tienkhanh1/spicy/main/LoadGame"))()
 else
     warn("No support PlaceId " .. game.PlaceId)
